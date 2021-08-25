34f712e8d1bacc50d16d0dbdc7d2e5191edbf2e1
MiguelDiPane <82734749+MiguelDiPane@users.noreply.github.com>
2021-08-25 18:47:33 -0300

    Update navegacion.php

--- Views/modules/navegacion.php
+++ Views/modules/navegacion.php
@@ -6,7 +6,7 @@
         Estos parametros van por el metodo GET-->
         <li><a href="index.php?action=inicio">Inicio</a></li>
         <li><a href="index.php?action=biblioratos">Biblioratos</a></li>
-        <li><a href="index.php?action=docentes">Docente</a></li>
+        <li><a href="index.php?action=docente">Docente</a></li>
         <li><a href="index.php?action=usuario">Usuario</a></li>
     </ul>
 </nav>
